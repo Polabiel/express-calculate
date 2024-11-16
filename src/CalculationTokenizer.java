6168803b871e10da2a00c647c66263d5cf744891
jorge <162496379+cc24319@users.noreply.github.com>
2024-11-16 10:36:41 -0300

    Add files via upload

--- src/CalculationTokenizer.java
+++ src/CalculationTokenizer.java
@@ -6,13 +6,20 @@ public class CalculationTokenizer {
     }
 
     public static void main(String[] args) throws Exception {
+
+        //deixei a main comentada temporariamente para poder tester o generateResults
         // String expressionInput = "45 + 7^2 * 3/76 -4 + (5 * 8) + 1";
-        String testeString = "10 + (2 * 3- 4) ^ 2/4 + 6 * 2";
-        String trimmedExpression = removeWhitespaces(testeString);
-        StringTokenizer tokenizer = new StringTokenizer(trimmedExpression, "+-*/^()", true);
+        //String testeString = "10 + (2 * 3- 4) ^ 2/4 + 6 * 2";
+        //String trimmedExpression = removeWhitespaces(testeString);
+        //StringTokenizer tokenizer = new StringTokenizer(trimmedExpression, "+-*/^()", true);
 
-        InfixToPostfixConverter object = new InfixToPostfixConverter(tokenizer);
+        //InfixToPostfixConverter object = new InfixToPostfixConverter(tokenizer);
 
-        object.convertToPostfix();
+        //object.convertToPostfix();
+
+        //essas 3 linhas foi pra testar o GeneratorResults
+        BufferQueue exemplo1 = new BufferQueue();
+        BufferStack exemplo2 = new BufferStack();
+        GeneratorResults a = new GeneratorResults(exemplo1, exemplo2);
     }
 }
--- src/GeneratorResults.java
+++ src/GeneratorResults.java
@@ -1,15 +1,19 @@
 class GeneratorResults {
   BufferQueue<String> queue;
   BufferStack<String> stack;
+  float v1 = 0;
+  float v2 = 0;
+  String op = "a";
 
   public GeneratorResults(BufferQueue<String> queue, BufferStack<String> stack) throws Exception {
-
+    System.out.println("inserindo variaveis");
     queue.guardeUmItem("10");
     queue.guardeUmItem("2");
     queue.guardeUmItem("3");
     queue.guardeUmItem("*");
     queue.guardeUmItem("4");
     queue.guardeUmItem("-");
+    queue.guardeUmItem("2");
     queue.guardeUmItem("^");
     queue.guardeUmItem("4");
     queue.guardeUmItem("/");
@@ -21,6 +25,51 @@ class GeneratorResults {
 
     this.queue = queue;
     this.stack = stack;
+    Float resultado;
+
+    while(!queue.isVazia()){
+      String removido = queue.recupereUmItem(); 
+      if (removido == "+" || removido =="*" || removido == "-" || removido =="/" || removido == "^"){
+        op = removido;
+      }
+      else{
+        stack.guardeUmItem(removido);
+      }
+      queue.removaUmItem();
+      if (op != "a" && op != null){
+        v1 = Float.parseFloat(stack.recupereUmItem());
+        stack.removaUmItem();
+        v2 = Float.parseFloat(stack.recupereUmItem());
+        stack.removaUmItem();
+        switch (op) {
+          case "-":
+            resultado = (v1 - v2);
+            stack.guardeUmItem(String.valueOf(resultado));
+            break;       
+          case "+":
+            resultado = (v1 + v2);
+            stack.guardeUmItem(String.valueOf(resultado));
+            break;
+          case "*":
+            resultado = (v1 * v2);
+            stack.guardeUmItem(String.valueOf(resultado));
+            break;        
+          case "/":
+            resultado = (v1 / v2);
+            stack.guardeUmItem(String.valueOf(resultado));
+            break;
+          case "^":
+            resultado = (float) (Math.pow(v2, v1));
+            stack.guardeUmItem(String.valueOf(resultado));
+            break;
+        }
+        op = null;
+      }
+    }
+    if (queue.isVazia()){
+      System.out.println("Queue vazia, o resultado da operação é:");
+      System.out.println(stack.recupereUmItem());
+    }
   }
 
 }
